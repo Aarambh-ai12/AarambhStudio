7f9dfaaa5fddc73192b334c63879723d2479fe8d
Aarambh-ai12 <aarambhfitandflow@gmail.com>
2025-08-01 10:41:33 +0000

    changes

--- src/assets/index.js
+++
@@ -1,6 +0,0 @@
-import bg1 from "../assets/img1.jpg"
-import logo from "../assets/logo.png"
-import trainee2 from "../assets/trainee2.jpg"
-import trainee1 from "../assets/trainee1.jpg"
-
-export { bg1, logo, trainee1, trainee2 }
